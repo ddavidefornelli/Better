c3c598bd7e34034cf09d3f6e3d14525e6177bb95
ddavidefornelli <fornelli.dv@gmail.com>
2025-09-13 11:11:39 +0200

    first commit

--- client/src/landingPage/LandingPage.jsx
+++ client/src/landingPage/LandingPage.jsx
@@ -7,7 +7,7 @@ export default function LandingPage() {
 
 function LandingPageHeader() {
     return(
-        <div className="mt-8 w-full mx-auto">
+        <div className="mt-8 w-full max-w-2xl mx-auto">
             <div className="flex justify-between items-center">
                 <h3 className="text-orange-500 mr-10">better</h3>
                 <h3 className="mr-10">accedi</h3>
