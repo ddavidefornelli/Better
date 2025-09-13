f10a07ab3fc619330aa58fa8d5d58b320a9fcbc6
ddavidefornelli <fornelli.dv@gmail.com>
2025-09-13 12:41:22 +0200

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
-import LandingPage from "./landingPage/LandingPage";
+import Contatti from "./Contatti";
+import LandingPage from "./LandingPage";
+import { Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
-        <div className="flex justify-center">
-            <div className="max-w-4xl">
-                <LandingPage/>
-            </div>
+      <div className="flex justify-center">
+        <div className="max-w-4xl w-full">
+    <Routes>
+          <Route path="/" element={<LandingPage />}></Route>
+          <Route path="/contatti" element={<Contatti />}></Route>
+    </Routes>
         </div>
-    )
+      </div>
+  )
 }
---
+++ client/src/Contatti.jsx
@@ -0,0 +1,5 @@
+export default function Contatti() {
+  return(
+  <h1>ciao siamo noi</h1>
+  )
+}
